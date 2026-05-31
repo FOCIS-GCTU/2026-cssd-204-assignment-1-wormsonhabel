b415b55b29065b17a7012d0ed8208ff3641d7eba
wormsonhabel <162710697+wormsonhabel@users.noreply.github.com>
2026-05-31 02:48:37 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: HNW.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Wormenor Habel Narh
+# STUDENT ID: 2425402501
+# Email: 2425402501@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Habel Narh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # This main function prints the user's initials 'HNW' in standard
+    # text format and as large, stylized block letters with periods.
+
+    print()
+    print("...HNW")
+    print()
+    print("...H.N.W.")
+    print()
+    print("HHHH      HHHH......NNNN      NNNN......WWWW      WWWW......")
+    print("HHHH      HHHH......NNNNNN    NNNN......WWWW      WWWW......")
+    print("HHHH      HHHH......NNNNNNNN  NNNN......WWWW      WWWW......")
+    print("HHHHHHHHHHHHHH......NNNN  NNNNNNNN......WWWW  WW  WWWW......")
+    print("HHHHHHHHHHHHHH......NNNN    NNNNNN......WWWW  WW  WWWW......")
+    print("HHHH      HHHH......NNNN      NNNN......WWWWWWWWWWWWWW......")
+    print("HHHH      HHHH......NNNN      NNNN......WWWWWWWWWWWWWW......")
+    print("HHHH      HHHH......NNNN      NNNN......WWWW  WW  WWWW......")
+    print("HHHH      HHHH..**..NNNN      NNNN..**..WWWW  WW  WWWW..**..")
+    print("HHHH      HHHH..**..NNNN      NNNN..**..WWWW      WWWW..**..")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
